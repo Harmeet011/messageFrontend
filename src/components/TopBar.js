dc0b24490ccd3f6c8f74c1477c874a074185bc21
Harmeet <hsdclashofclans@gmail.com>
2024-08-05 14:18:28 +1200

    fixed conflicts

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -26,22 +26,8 @@ function TopBar() {
           >
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/chatroom">ChatRoom</Nav.Link>
-            <Nav.Link href="/sumNumber">Sum Numbers</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
             <Nav.Link href="/createChatroom">Create Chatroom</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Button variant='outline-danger' onClick={logoutUser}>Logout</Button>
