543b76b1c7baf92fe857a64cff9341c1fb109f30
Harmeet <hsdclashofclans@gmail.com>
2024-08-05 21:07:06 +1200

    fixed code review comments

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -3,11 +3,15 @@ import { BaseUrl } from "../Constants";
 import axios from "axios";
 import localStorageService from "../LocalStorageService";
 import Table from "react-bootstrap/Table";
+import { Spinner } from "react-bootstrap";
 
 export default function ChatRoom() {
   const [chatrooms, setChatrooms] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState({});
 
   useEffect(() => {
+    setIsLoading(true);
     const token = localStorageService.get("auth-token");
     let config = {
       method: "get",
@@ -21,7 +25,30 @@ export default function ChatRoom() {
     axios
       .request(config)
       .then((response) => {
+        setIsLoading(false);
         setChatrooms(response.data);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+      });
+
+      let userConfig = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: BaseUrl + 'chat/users/',
+        headers: { 
+            Authorization: `token ${token}`,
+        }
+      };
+      
+      axios.request(userConfig)
+      .then((response) => {
+        const tempUser = {}
+        response.data.forEach((user) => {
+          tempUser[user.id] = user.username
+        })
+        setUsers(tempUser)
       })
       .catch((error) => {
         console.log(error);
@@ -36,17 +63,33 @@ export default function ChatRoom() {
           <tr>
             <th>Id</th>
             <th>Chatroom Name</th>
-            <th>Members</th>
+            <th>Chatroom Members</th>
             <th>Created By</th>
           </tr>
         </thead>
         <tbody>
+          {isLoading && (
+            <tr>
+              <td colSpan="4">
+                {" "}
+                <Spinner
+                  className="ms-2"
+                  animation="border"
+                  role="status"
+                  size="sm"
+                >
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+                Loading...
+              </td>
+            </tr>
+          )}
           {chatrooms.map((chatroom) => (
             <tr key={chatroom.id}>
               <td>{chatroom.id}</td>
               <td>{chatroom.name}</td>
-              <td>{chatroom.members}</td>
-              <td>{chatroom.created_by}</td>
+              <td>{chatroom.members.map((member) => users[member]).join(", ")}</td>
+              <td>{users[chatroom.created_by]}</td>
             </tr>
           ))}
         </tbody>
